4bfd2076ef9d0a2e6e3f614438c4265a1b642ab0
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-22 14:10:55 -0600

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -2,3 +2,5 @@ fun main() {
     while (true) {
         println("\n===== MENÚ PRINCIPAL =====")
         println("1. Calcular superficie de un cuadrado")
+        println("2. Calcular total de una compra")
+        println("3. Calcular índice de masa corporal (IMC)")
